3245ca9275f0fa39e5eabfc29605d895ffb646f8
Kyle Lemons <kal18c@dmc-207-15.local>
2019-05-01 08:44:49 -0500

    added data variables to hold the content of each json

--- MobileDevFinalProject/BaseBallDataController.swift
+++ MobileDevFinalProject/BaseBallDataController.swift
@@ -14,4 +14,10 @@ class BaseBallDataController {
     let franchisePath = Bundle.main.path(forResource: "teamFranchises", ofType: "json");
     let battingPath = Bundle.main.path(forResource: "batting", ofType: "json");
     let pitchingPath = Bundle.main.path(forResource: "pitching", ofType: "json");
+    
+    lazy var playerData = try? NSData(contentsOfFile: playerPath!, options: NSData.ReadingOptions.mappedIfSafe);
+    lazy var teamData = try? NSData(contentsOfFile: teamPath!, options: NSData.ReadingOptions.mappedIfSafe);
+    lazy var franchiseData = try? NSData(contentsOfFile: franchisePath!, options: NSData.ReadingOptions.mappedIfSafe);
+    lazy var battingData = try? NSData(contentsOfFile: battingPath!, options: NSData.ReadingOptions.mappedIfSafe);
+    lazy var pitchingData = try? NSData(contentsOfFile: pitchingPath!, options: NSData.ReadingOptions.mappedIfSafe);
 }
